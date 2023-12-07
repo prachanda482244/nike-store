cdb702e8cf1bb920fcae1acb140ccbb49877828e
Prachanda Rana <62231260+prachanda482244@users.noreply.github.com>
2023-12-07 13:55:37 +0545

    nike store services

--- nike_store/src/sections/Services.jsx
+++ nike_store/src/sections/Services.jsx
@@ -1,8 +1,13 @@
+import ServiceCard from "../Components/ServiceCard"
+import { services } from "../constants"
+
 const Services = () => {
     return (
-        <div>
-            services
-        </div>
+        <section className='max-container flex justify-center flex-wrap gap-9'>
+            {services.map((service) => (
+                <ServiceCard key={service.label} {...service} />
+            ))}
+        </section>
     )
 }
 
